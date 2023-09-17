b7c8fc0eb5119c6d10ec7d8463832a04321654d7
Jacobo Flores Perez <144180440+JacoboFlores@users.noreply.github.com>
2023-09-17 14:57:42 -0600

    Add files via upload

---
+++ P6_1s21.java
@@ -0,0 +1,9 @@
+class p6_1s21{
+	public static void main(String[] args) {
+		String nombre="victor";
+		int edad=23;
+		System.out.println("El valor para el nombre es" + nombre);
+		System.out.println("la edad es" + edad);
+		System.out.println("puede seguir programando");
+	}
+}
